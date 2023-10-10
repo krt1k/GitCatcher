78da699e5867d48df6e5c6c300f841c0b827f2dd
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-10-10 20:25:33 +0530

    copy inti.sh to /etc dir

--- init.sh
+++ init.sh
@@ -8,7 +8,6 @@ fi
 
 # Creating the directory where the script will be stored
 mkdir -p /etc/GitCatcher
-cp -r . /etc/GitCatcher/
 
 # Specify the Git repository URL
 read -p "Enter the git repo url that you want to sync: " git_url
@@ -16,6 +15,9 @@ read -p "Enter the git repo url that you want to sync: " git_url
 # Add the git repo url to the script
 sed -i "4i GIT_REPO_URL=$git_url" /etc/GitCatcher/run.sh
 chmod +x /etc/GitCatcher/run.sh
+
+# Copy the script to the /etc directory
+cp -r . /etc/GitCatcher/
 
 # create a cronjob that runs the script every hour
 echo "0 * * * * root /etc/GitCatcher/run.sh" >> /etc/crontab
