86cbcc5a878e20003017ea3e69087c48f93d1623
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-10-16 15:49:09 +0530

    added uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+rm -rf /etc/GitCatcher
+
+# remove the last line of /etc/crontab
+sed -i '$ d' /etc/crontab
