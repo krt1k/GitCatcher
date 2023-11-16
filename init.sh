5432a7573718cb36824ae70afd870a140df254cf
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-16 12:51:48 +0530

    commitHash to commitMessage

--- init.sh
+++ init.sh
@@ -44,6 +44,6 @@ echo "export rentlyEmail=\"${rentlyEmail}\"" >> /etc/environment
 source /etc/environment
 
 # create a cronjob that runs the script every three hour
-echo "$current_minute * * * * root /usr/bin/run" >> /etc/crontab 
+echo "* * * * * root /usr/bin/run" >> /etc/crontab 
 
 # script v1.11
