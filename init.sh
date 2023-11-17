cede97f69ac6fb64c1b6df494c47a1fca7e5c66a
Kishorekarthik P <kishorekarthik98@gmail.com>
2023-11-17 17:35:46 +0530

    v1.13

--- init.sh
+++ init.sh
@@ -46,4 +46,4 @@ source /etc/environment
 # create a cronjob that runs the script every three hour
 echo "$current_minute */3 * * * root /usr/bin/run" >> /etc/crontab 
 
-# script v1.12
+# script v1.13
